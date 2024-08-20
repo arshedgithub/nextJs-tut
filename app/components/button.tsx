ab1f82b5db31a787430b0b4d1559dd18a03e1d2b
arshedgithub <arshedahmedmail@gmail.com>
2024-08-20 17:54:11 +0530

    client side component

---
+++ app/components/button.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button onClick={() => console.log('clicked')}>Client side event</button>
+)
+}
+
+export default Button
